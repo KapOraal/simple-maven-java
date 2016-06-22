d67e42d939c204be35f0c76e1860a0b87af2bd94
KapOraal <djibril.dieng@isen-lille.fr>
2016-06-22 11:28:20 +0200

    Quality Sonar

--- src/main/java/example/One.java
+++ src/main/java/example/One.java
@@ -4,12 +4,11 @@ import java.util.logging.Logger;
 
 public class One {
   String message = "foo";
+  private static Logger logger = Logger.getLogger("example.One");
 
   public String foo() {
     return message;
   }
-
-  private static final Logger logger = Logger.getLogger("example.One");
 
   public void uncoveredMethod() {
     logger.info("foo");
